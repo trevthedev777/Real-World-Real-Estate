9a8be8f39a57fbbf3eaf0fe3b26819fc45113d7f
Your Name <bloodsportrecords@gmail.com>
2022-12-01 15:44:30 +0000

    update SECRET_KEY

---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+
+os.environ['SECRET_KEY'] = 's215hpcrb_rsznj3++_0-g$_fbxu55_34khi ^ xa7-u09( & yza*'
--- realworldrealestates/settings.py
+++ realworldrealestates/settings.py
@@ -10,6 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
+if os.path.isfile('env.py'):
+    import env
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h1b(_y=ud578_#+ckhlx2-k)%&05=jl@0c5+)%ow_4he4#4wr4'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['real-world-real-estates.herokuapp.com/', 'localhost']
 
 
 # Application definition
