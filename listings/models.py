4196f08919eb22386ed106528309bf4c64a12ed5
Your Name <bloodsportrecords@gmail.com>
2022-12-03 16:01:35 +0000

    Create listing and realtors model and migrate to database

--- listings/models.py
+++ listings/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from datetime import datetime
+from realtors.models import Realtor
 
-# Create your models here.
+
+# DO_NOTHING = keeps the listing even if the realtor is deleted
+class Listing(models.Model):
+    realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=50, unique=True)
+    address = models.CharField(max_length=200, unique=True)
+    city = models.CharField(max_length=200)
+    county = models.CharField(max_length=20)
+    postcode = models.CharField(max_length=8)
+    description = models.TextField(blank=True)
+    price = models.IntegerField()
+    bedrooms = models.IntegerField()
+    bathrooms = models.DecimalField(max_digits=3, decimal_places=1)
+    garage = models.IntegerField(default=0)
+    sqm = models.IntegerField()
+    lot_size = models.DecimalField(max_digits=6, decimal_places=1)
+    is_published = models.BooleanField(default=True)
+    list_date = models.DateField(default=datetime.now, blank=True)
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+
+    # The main field
+    def __str__(self):
+        return self.title
--- realtors/models.py
+++ realtors/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from datetime import datetime
 
-# Create your models here.
+
+class Realtor(models.Model):
+    name = models.CharField(max_length=20)
+    photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
+    description = models.TextField(blank=True)
+    email = models.CharField(max_length=30)
+    phone = models.CharField(max_length=15)
+    is_mvp = models.BooleanField(default=False)
+    hire_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    # The main field
+    def __str__(self):
+        return self.name
