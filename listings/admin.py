f104081aa7d24eb91a3d02f8e089301b2b658439
Your Name <bloodsportrecords@gmail.com>
2022-12-03 20:16:17 +0000

    Create admin features for listing admin panel

--- listings/admin.py
+++ listings/admin.py
@@ -2,5 +2,18 @@ from django.contrib import admin
 
 from .models import Listing
 
-# Register the model
-admin.site.register(Listing)
+
+class ListingAdmin(admin.ModelAdmin):
+    # shows the fieldssto display on admin panel
+    # set the clickable links with lis_display
+    list_display = ('id', 'title', 'is_published',
+                    'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+    list_filter = ('realtor',)
+    list_editable = ('is_published',)
+    search_fields = ('title', 'description', 'address',
+                     'city', 'county', 'postcode', 'price')
+    list_per_page = 10
+
+    # Register the model
+admin.site.register(Listing, ListingAdmin)
