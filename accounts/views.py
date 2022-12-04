e3f9795a5290230b49eb89fba7d054d2e49d9397
Your Name <bloodsportrecords@gmail.com>
2022-12-04 22:15:25 +0000

    register a account functionality and validation

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,38 @@
 from django.shortcuts import render, redirect
-from django.contrib import messages
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
 
 
 def register(request):
     # Register User
     if request.method == 'POST':
-        messages.error(request, 'Testing error message')
-        return redirect('register')
+        # Get form values
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        # Check if passwords match
+        if password == password2:
+            # check password
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'That username is taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'That username is taken')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name=last_name)  # noqa
+                    # Login after registering user
+                    auth.login(request, user)
+                    messages.success(request, 'Successfully logged in')
+                    return redirect('index')
+        else:
+            messages.error(request, 'Passwords do not match!')
+            return redirect('register')
     else:
         return render(request, 'accounts/register.html')
 
