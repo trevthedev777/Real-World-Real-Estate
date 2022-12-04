ef94eae06c9e273db7f97b096716da61cc15b258
Your Name <bloodsportrecords@gmail.com>
2022-12-04 18:48:30 +0000

    Create and route register and login html pages

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,9 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login', views.login, name='login'),  # Login page
-    path('register', views.register,
-         name='register'),  # listing page
+    path('login', views.login, name='login'),
+    path('register', views.register, name='register'),
     path('logout', views.logout, name='logout'),
-    path('dashboard', views.dashboard, name='dashboard'),
+    path('dashboard', views.dashboard, name='dashboard')
 ]
