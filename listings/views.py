44d71153253f085292a702b1dbf9f068db0ec7fb
Your Name <bloodsportrecords@gmail.com>
2022-12-04 11:43:41 +0000

    Create pagination, filter and sort by functionality

--- listings/views.py
+++ listings/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from listings.models import Listing
 
 
 def index(request):
-    listings = Listing.objects.all()
+    listings = Listing.objects.order_by(
+        '-list_date').filter(is_published=True)  # sort by most recent
+    paginator = Paginator(listings, 3)  # sort listings, 3 per page
+    page = request.GET.get('page')  # calls the page from the back end
+    paged_listings = paginator.get_page(page)
 
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
     return render(request, 'listings/listings.html', context)
 
